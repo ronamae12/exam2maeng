0be4b2f947be39a853360b99a4067e85d619339c
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-11-04 18:50:42 +0800

    Initial Commit

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+let subjectTitle = " DSA";  
+let classSchedule = "10:00 AM - 11:30 AM, Mon/Fri";  
+let classroom = "COR A";  
+let classInstructor = "Vitruvius John Barayuga";  
+let studentName = "Rona Mae Ancheta";  
+
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
